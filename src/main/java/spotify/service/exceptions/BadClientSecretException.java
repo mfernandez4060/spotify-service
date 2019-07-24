47ab53a51075283f08d4f73abbf1bbc49aef70b8
mfernandez <mail.mfernandez@gmail.com>
2019-07-24 10:20:00 -0300

    refactor test

--- src/main/java/spotify/service/exceptions/BadClientSecretException.java
+++ src/main/java/spotify/service/exceptions/BadClientSecretException.java
@@ -8,8 +8,8 @@ public class BadClientSecretException extends CustomException {
 
 	private static final long serialVersionUID = 7806088387159169700L;
 
-	public BadClientSecretException(String clientId) {
-        super("Bad clientId [" + clientId + "]");
+	public BadClientSecretException(String clientSecret) {
+        super("Bad ClientSecret [" + clientSecret + "]");
     }
 
 }
